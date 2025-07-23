bece82e5ecf01b49bc23bbd7a3d00a845e0234a3
“Bhargav <nbhargav0098@gmail.com>
2025-07-23 17:11:48 +0530

    learned about routing and used postman to test the http methods

--- src/app.js
+++ src/app.js
@@ -2,11 +2,23 @@ const express = require("express");
 
 const app = express();
 
-app.use("/test",(req,res)=>{
-  res.send("Hello Bhargav")
+app.get("/user",(req,res)=>{
+  res.send({firstName: "Bhargav", lastName: "Nelakuditi"})
 })
 
-app.use("/newTab",(req,res)=>{
+app.post("/user",(req,res)=>{
+  console.log(req.body);
+  //saving to DB
+  res.send("Data successfully saved to DB!")
+})
+
+app.delete("/user",(req,res)=>{
+  res.send("Successfully deleted the user from DB!")
+})
+
+
+//this will match to all the HTTP method API calls to /newTab and order of using this will always matter ie if this is used in the starting then no matter which route post or get or put or delete of /user is called this will be executed.
+app.use("/user",(req,res)=>{
   res.send("This is new tab")
 })
 
