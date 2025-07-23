6d31747f108d26fa313823c3b712fdbe2ae30802
“Bhargav <nbhargav0098@gmail.com>
2025-07-23 15:57:25 +0530

    creaded server using Express

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.use("/test",(req,res)=>{
+  res.send("Hello Bhargav")
+})
+
+app.use("/newTab",(req,res)=>{
+  res.send("This is new tab")
+})
+
+app.listen(3000, ()=>{ console.log("server is listening on 3000 port")});
