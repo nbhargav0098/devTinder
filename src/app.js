b2e2dcac298526aa96618fa1625f3a67058ff449
“Bhargav <nbhargav0098@gmail.com>
2025-07-25 17:20:04 +0530

    1)Installed mongoose 2)connected to devTinder DB 3)connected db first and then connected server 4)Used http post method and wrote a middleware to get data from DB and return response to client

--- src/app.js
+++ src/app.js
@@ -1,22 +1,27 @@
 const express = require("express");
-
+const connectDb = require("./config/database");
 const app = express();
+const User = require("./models/user");
 
-const { adminAuth, userAuth } = require("./middlewares/auth");
-
-//this will match to all the HTTP method API calls to /newTab and order of using this will always matter ie if this is used in the starting then no matter which route post or get or put or delete of /user is called this will be executed.
-app.use("/admin", adminAuth);
-
-app.get("/user/userLogin", userAuth, (req,res)=>{
-  res.send("fetched user data!")
-});
-
-app.get("/admin/adminDetails", (req,res)=>{
-  res.send("fetched admin details");
+app.post("/signup", async (req,res)=>{
+  const userObj = {
+    firstName: "Bhargav",
+    lastName: "Nelakuditi",
+    emailId: "nbhargav98@gmail.com",
+    password: "testing1234"
+  }
+  const user = new User(userObj); //creates a new instance of user 
+  try {
+    await user.save(); //saves the new user into the db
+    res.send("user has been successfully created!")
+  } catch (error) {
+    res.status(400).send("Error saving the user!", error.message)
+  }
 })
 
-app.delete("/admin/deleteAdmin", (req,res)=>{
-  res.send("removed admin");
+connectDb().then(()=>{
+  console.log("data base successfully connected")
+  app.listen(3000, ()=>{ console.log("server is listening on 3000 port")});
+}).catch((err)=>{
+  console.error("error",err)
 })
-
-app.listen(3000, ()=>{ console.log("server is listening on 3000 port")});
---
+++ src/config/database.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+
+const connectDb = async() => {
+  await mongoose.connect("mongodb+srv://nbhargav0098:N794Qozlnxis7OZz@mynode.lhcqbn6.mongodb.net/devTinder");
+} 
+
+module.exports = connectDb;
